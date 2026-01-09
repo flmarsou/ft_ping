1b6f8ca87c681d3a52f4a38905a7a26a9fa0084a
flmarsou <marsouinflorent12@gmail.com>
2026-01-09 15:19:37 +0100

    Add initialization and parser

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include "ft_ping.h"
+
+static void	init_ping(t_ping *ping)
+{
+	ping->audible = false;
+	ping->count = 0;
+	ping->date = false;
+	ping->interval = 0;
+	ping->quiet = false;
+	ping->size = 0;
+	ping->verbose = false;
+	ping->destination = ((void*)0);
+}
+
+i32	main(const i32 argc, const i8 **argv)
+{
+	t_ping	ping;
+
+	init_ping(&ping);
+
+	if (!parse_arguments(argc, argv, &ping))
+		return (1);
+
+	return (0);
+}
